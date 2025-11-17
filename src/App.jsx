068183bf8ce99ac73ff2d267a8dd73f0e7582add
Shiva Vishwakarma <shivavishwakarma9291@gmail.com>
2025-11-17 22:53:38 +0530

    Create a User Function and Export

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./Header"
+import User from "./User"
 function Apple(){
   return (
     <div>
       <Header/>
+      <User/>
     <h1>This is my First function</h1>
     </div>
   )
