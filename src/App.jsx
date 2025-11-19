092dc0c9033dbe1c566fe2e95fb49d0a1d3b573c
Shiva Vishwakarma <shivavishwakarma9291@gmail.com>
2025-11-19 23:48:00 +0530

    normal function and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import { useState } from "react"
+import Header from "./Header"
+import Banner from "./Banner"
 
-function Apple(){
-  const [counter,setCounter]=useState(0)
-  return (
+// function Apple(){
+//   const [counter,setCounter]=useState(0)
+//   return (
+//     <div>
+//       <Header/>
+//       <Banner/>
+//       <h1>Counter Value : {counter}</h1>
+//       <button onClick={()=>setCounter(counter+1)}>Incress Value</button>
+//       <button onClick={()=>setCounter(counter-1)}>Decress Value</button>
+//     </div>
+//   )
+// }
+
+function Counting(){
+  const [number , setNumber] = useState(0)
+  return(
     <div>
-      <h1>Counter Value : {counter}</h1>
-      <button onClick={()=>setCounter(counter+1)}>Incress Value</button>
-      <button onClick={()=>setCounter(counter-1)}>Decress Value</button>
+      <Car/>
+      <h1>Counting Value : {number}</h1>
+      <button onClick={()=>setNumber(number+2)}>+2</button>
+      <button onClick={()=>setNumber(number-2)}>-2</button>
+
     </div>
   )
 }
-export default Apple
+
+
+function Car(){
+  return(
+    <div>
+      <h1>shiva</h1>
+    </div>
+  )
+}
+export default Counting
---
+++ src/Banner.jsx
@@ -0,0 +1,10 @@
+function Banner() {
+    return (
+        <div>
+            <h1>Banner Section</h1>
+        </div>
+    )
+    
+}
+
+export default Banner
